5840a7d59f0d93256af9e2e7af3804f671267a1e
Olivia Kesler <oakesler@protonmail.ch>
2019-12-24 08:24:06 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed, :age 
   
   @@dogs = [ ]
-  def intialize(name, breed, age)
+  def initialize(name, breed, age)
     @name = name 
     @breed = breed 
     @age = age
